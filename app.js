2d2924625d0424b556223aab763544fd0fd2d031
pasindu <pasinduprabhath@gmail.com>
2023-05-30 00:33:36 +0530

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRouter = require('./api/user/user.router');
 app.use(express.json());
 
 app.use('/api/user', userRouter);
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {//dfd
     console.log("Server up and running on PORT :", process.env.PORT);
     }
 );
