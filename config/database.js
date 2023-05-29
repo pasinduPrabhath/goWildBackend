0cd6a4c770388800cd5ce0b923cbe5f4b8bd8748
pasindu <pasinduprabhath@gmail.com>
2023-05-30 01:08:55 +0530

    .

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const {createPool} = require('mysql2');
 
 const pool = createPool({
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database : process.env.MYSQL_DB,
+    port: 3306,
+    host: 'localhost',
+    user: '',
+    password: '',
+    database : 'gowild',
     connectionLimit: 10
 });
 
