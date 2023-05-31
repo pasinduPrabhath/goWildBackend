3c7ed028d566273696a84d8b67f1b8e17492d082
pasindu <pasinduprabhath@gmail.com>
2023-05-31 19:18:08 +0530

    .

--- config/database.js
+++ config/database.js
@@ -6,7 +6,12 @@ const pool = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database : process.env.MYSQL_DB,
-    connectionLimit: 10
+    connectionLimit: 10,
+    ssl: {
+        ca: fs.readFileSync('/path/to/ca-cert.pem'),
+        cert: fs.readFileSync('/path/to/client-cert.pem'),
+        key: fs.readFileSync('/path/to/client-key.pem')
+      }
 });
 
 module.exports = pool;
