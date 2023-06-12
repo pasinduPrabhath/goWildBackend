ef0e89848c8030282394e6531315c7af3a1d346b
pasindu <pasinduprabhath@gmail.com>
2023-06-12 22:44:47 +0530

    .

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -38,6 +38,7 @@ module.exports = {
         }
         const user = results[0];
         const passwordMatch = bcrypt.compareSync(password, user.password);
+        console.log('saved one'+ password, + 'entered one' + user.password);
         if (!passwordMatch) {
             return res.json({
             success: 0,
