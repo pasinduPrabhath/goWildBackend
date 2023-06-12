8651212424c1ae3e50fe7a7932c0709343307eb8
pasindu <pasinduprabhath@gmail.com>
2023-06-12 23:51:16 +0530

    .

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -4,37 +4,33 @@ const bcrypt = require('bcrypt');
 module.exports = {
     
     create: (data, callBack) => {
-        const saltRounds = 10;
-        bcrypt.hash(data.password, saltRounds, (err, hash) => {
-            if (err) {
-                return callBack(err);
-            }
-            data.password = hash;
-            pool.query(
-                `insert into user(firstName,lastName,email,password,birthday,country,town,mobileNumber,gender,sp,nicNumber,userRole,timestamp) values(?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-                [
-                    data.firstName,
-                    data.lastName,
-                    data.email,
-                    data.password,
-                    data.birthday,
-                    data.country,
-                    data.town,
-                    data.mobileNumber,
-                    data.gender,
-                    data.sp,
-                    data.nicNumber,
-                    data.userRole,
-                    data.timestamp
-                ],
-                (error, results, fields) => {
-                    if(error){
-                        return callBack(error);
-                    }
-                    return callBack(null, results);
+        if(!data.firstName){
+            return callBack("First Name is required");
+        }
+        pool.query(
+            `insert into user(firstName,lastName,email,password,birthday,country,town,mobileNumber,gender,sp,nicNumber,userRole,timestamp) values(?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+            [
+                data.firstName,
+                data.lastName,
+                data.email,
+                data.password,
+                data.birthday,
+                data.country,
+                data.town,
+                data.mobileNumber,
+                data.gender,
+                data.sp,
+                data.nicNumber,
+                data.userRole,
+                data.timestamp
+            ],
+            (error, results, fields) => {
+                if(error){
+                    return callBack(error);
                 }
-            );
-        });
+                return callBack(null, results);
+            }
+        );
     },
     checkLoginEmail: (data, callBack) => {
         pool.query(
