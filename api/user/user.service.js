f5b5508e07ef895af6e6f4f816ebec533e13c797
pasindu <pasinduprabhath@gmail.com>
2023-06-25 14:48:32 +0530

    .

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -81,7 +81,8 @@ module.exports = {
             `select user_details.firstName,user_details.lastName,user_details.country,user_details.email,user_details.birthday,user_details.town,user_details.gender,service_provider.nicNumber,service_provider.user_id_img_front,service_provider.user_id_img_rear,service_provider.is_approved,user_details.user_id 
             FROM user_details
             INNER JOIN service_provider
-            ON user_details.user_id = service_provider.user_id`,
+            ON user_details.user_id = service_provider.user_id
+            where service_provider.is_approved = "pending"`,
             (error, results) => {
                 if (error) {
                     if (typeof callBack === 'function') {
