6b73dacdef54d8fa3922d5a9ed4488f11ab1973e
pasindu <pasinduprabhath@gmail.com>
2023-07-02 22:28:59 +0530

    .

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const adminRouter = require('./api/admin/admin.router');
 
 app.use(express.json());
 app.use('/api/user', userRouter);
-app.use('/api/admin', serviceProviderRouter);
-app.use('/api/serviceProvider', adminRouter);
+app.use('/api/admin', adminRouter );
+app.use('/api/serviceProvider', serviceProviderRouter);
 
 app.listen(process.env.PORT, () => {//dfd
     console.log("Servers up and running on PORT :", process.env.PORT);
