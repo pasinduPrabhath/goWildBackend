ae1f58579c90e83c2f1ada1d631fcbc333c5a6ba
pasindu <pasinduprabhath@gmail.com>
2023-07-03 22:11:15 +0530

    .

--- api/admin/admin.router.js
+++ api/admin/admin.router.js
@@ -1,8 +1,9 @@
-const{getTotalUsers,getServiceProvider,approveServiceProvider,deleteTheDeclinedEntry} = require('./admin.controller');
+const{getTotalUsers,getServiceProvider,approveServiceProvider,deleteTheDeclinedEntry,getTotalServiceProviders} = require('./admin.controller');
 const router = require('express').Router();
 
 router.get('/totalUsers', getTotalUsers);
 router.get('/getServiceProvider', getServiceProvider);
+router.get('/getServiceProvider', getTotalServiceProviders);
 router.post('/approveServiceProvider', approveServiceProvider);
 router.post('/deleteDeclinedEntry', deleteTheDeclinedEntry);
 module.exports = router;
